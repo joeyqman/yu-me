75061f0afe3305c4d59361c764a877c1442ffe81
joeyqman <55720352+joeyqman@users.noreply.github.com>
2021-05-16 21:37:17 +0200

    Add files via upload

---
+++ yume.js
@@ -0,0 +1,21 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+//console.log('Hier worden de ijsjes in elkaar gezet');
+
+
+window.onscroll = function () {
+    myFunction()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
